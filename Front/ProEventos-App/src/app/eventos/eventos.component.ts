a6ef574c1985e740d7a9c935d89d366a90262b9e
Renato Carvalho <renato@resolvendobug.com.br>
2023-01-30 23:43:10 -0300

    corrigido .gitignore

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -4,41 +4,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent {
+  public eventos: any = [];
 
-  public eventos: any;
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getEventos();
   }
 
   public getEventos(): void {
-
     this.http.get('http://localhost:5093/api/Evento').subscribe(
-      response =>
-      this.eventos = response,
-      error => console.log(error)
-
+      (response) => (this.eventos = response),
+      (error) => console.log(error)
     );
-
-    // this.eventos =  [
-    //   {
-    //     tema: '.Net',
-    //     local: 'Rio de Janeiro'
-    //   },
-    //   {
-    //     tema: 'Angular',
-    //     local: 'São Paulo'
-    //   },
-    //   {
-    //     tema: 'Angular 11',
-    //     local: 'São Paulo'
-    //   }
-
-    //   ]
   }
 }
