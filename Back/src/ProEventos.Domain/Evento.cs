8c9b99f47a097989528d6f49ff201c77ca46e4ee
Renato Carvalho <renato@resolvendobug.com.br>
2023-02-01 23:30:08 -0300

    feat:Application,Domain,Presistence

---
+++ Back/src/ProEventos.Domain/Evento.cs
@@ -0,0 +1,21 @@
+
+namespace ProEventos.Domain
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string? Local { get; set; }
+        public DateTime? DataEvento { get; set; }
+        public string? Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string? ImagemURL { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+        public IEnumerable<Lote>? Lote { get; set; }
+        public IEnumerable<RedeSocial>? RedeSociais { get; set; }
+
+        public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
+
+
+    }
+}
